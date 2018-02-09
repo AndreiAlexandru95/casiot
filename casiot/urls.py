5dd5ecada73d750661e244c81793b412667ec372
Andrei Alexandru <aa15g14@soton.ac.uk>
2018-02-09 16:42:33 +0000

    Can register Users

--- casiot/urls.py
+++ casiot/urls.py
@@ -18,10 +18,14 @@ from django.urls import path, include
 
 from django.conf import settings
 
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('interface.urls')),
+    path('register/', CreateView.as_view(template_name='account/register.html', form_class=UserCreationForm, success_url='/')),
 ]
 
 if settings.DEBUG:
