edaf9a87bed91bc0877b241228739954703bfb9d
Andrei Alexandru <andrei@CASCODA211>
2018-02-22 14:46:23 +0000

    Changed the redis-server port to 8001 to not conflict with C211 port config

--- casiot/settings.py
+++ casiot/settings.py
@@ -141,7 +141,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [("localhost", 8001)],
         },
     },
 }
