6f9cf28539f3008e2cf330945839bef500d9c0fc
Andrei Alexandru <andrei@CASCODA211>
2018-04-05 12:07:52 +0100

    Changed react ip to mimic rpi

--- components/dashboard/index.jsx
+++ components/dashboard/index.jsx
@@ -8,7 +8,7 @@ import Dash from './Dash.jsx'
 var device_sock = 'ws://'+window.location.host+'/devices/'
 var current_user = null
 
-$.get('http://192.168.10.201:8000/api/user/?format=json', function(result){
+$.get('http://192.168.10.207:8000/api/user/?format=json', function(result){
 	current_user = result
 	render_component()
 })
